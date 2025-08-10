b5f2d22e20525014b52fbc75ff98c0e74a42c53e
Jia Le <jle26531@gmail.com>
2025-08-10 22:27:07 +0800

    remove unsed error import

---
+++ pages/blog/index.jsx
@@ -0,0 +1,14 @@
+import Layout from "@/layouts/globals";
+
+export default function Blog() {
+    return (
+        <Layout
+        navTitle={'Blog'}
+        path={'/blog'}
+        typeOfPage={'blog'}
+        title={'Jia Le\'s Blog'}
+        description={"A wonderful memories and moments, captured through Jia Le's eyes. - Jia Le's Blog"}>
+
+        </Layout>
+    )
+}
